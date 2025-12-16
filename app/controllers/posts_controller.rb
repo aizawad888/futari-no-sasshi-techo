8a1d85e8f68af599c18a1e40cb4a474c02cb2538
aizawad888 <201208476+aizawad888@users.noreply.github.com>
2025-12-16 13:13:01 +0900

    refactor:Lintチェック

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_post!, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :authorize_post!, only: [ :edit, :update, :destroy ]
 
   def new
     @post = Post.new
